56ce3f7a77bd0a650d357cee5c813b2a1e95c1ea
Dongye Meng <dmeng@cs.unc.edu>
2012-11-13 22:44:42 -0500

    Add JUnit test

--- test/semanticMarkup/ling/learn/UnsupervisedClauseMarkupTest.java
+++ test/semanticMarkup/ling/learn/UnsupervisedClauseMarkupTest.java
@@ -1,5 +1,8 @@
 package semanticMarkup.ling.learn;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 //import java.util.HashMap;
 import java.util.List;
@@ -7,38 +10,30 @@ import java.util.Map;
 import java.util.Set;
 
 import semanticMarkup.core.Treatment;
-
-
-import org.junit.Test;
-
+import semanticMarkup.ling.learn.UnsupervisedClauseMarkup;
 
 public class UnsupervisedClauseMarkupTest {
 	
 	@Test
 	public void testUnsupervisedClauseMarkup() {
 		String str = "Users/nescent/Phenoscape/TEST2/target/descriptions";
-		UnsupervisedClauseMarkup t = new UnsupervisedClauseMarkup(str,"biocreative2012","plain","test");
-		t.populatesents();
-		
-		List<Treatment> l_test = new ArrayList<Treatment>();
-		//Map<String, String> m_test = new HashMap<String,String>();
-		
-		
-		t.learn(l_test);
-		t.getSentences();
-		t.getSentencesForOrganStateMarker();
-		t.getAdjNouns();
-		t.getAdjNounSent();
-		t.getSentenceTags();
-		t.getBracketTags();
-		t.getWordRoleTags();
-		t.getWordToSources();
-		t.getRoleToWords();
-		t.getWordsToRoles();
-		t.getHeuristicNouns();
+		List<Treatment> treatments_l = new ArrayList<Treatment>();
+				
+		UnsupervisedClauseMarkup tester = new UnsupervisedClauseMarkup(str,"biocreative2012","plain","test");
+		assertEquals("Result", null, tester.getAdjNouns());
+		assertEquals("Result", null, tester.getAdjNounSent());
+		assertEquals("Result", null, tester.getBracketTags());
+		assertEquals("Result", null, tester.getHeuristicNouns());
+		assertEquals("Result", null, tester.getRoleToWords());
+		assertEquals("Result", null, tester.getSentences());
+		assertEquals("Result", null, tester.getSentencesForOrganStateMarker());
+		assertEquals("Result", null, tester.getSentenceTags());
+		assertEquals("Result", null, tester.getTermCategories());
+		assertEquals("Result", null, tester.getWordRoleTags());
+		assertEquals("Result", null, tester.getWordsToRoles());
+		assertEquals("Result", null, tester.getWordToSources());
 
-		t.getTermCategories();
-
-
+		
+		
 	}
 }
