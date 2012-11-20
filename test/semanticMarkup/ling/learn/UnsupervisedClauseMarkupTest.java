8773afdd94e60280fa4c97f0ed98a5c0c49ea0d2
Dongye Meng <dmeng@cs.unc.edu>
2012-11-20 17:15:24 -0500

    test sentenceLoader.

--- test/semanticMarkup/ling/learn/UnsupervisedClauseMarkupTest.java
+++ test/semanticMarkup/ling/learn/UnsupervisedClauseMarkupTest.java
@@ -11,12 +11,13 @@ import java.util.Set;
 
 import semanticMarkup.core.Treatment;
 import semanticMarkup.ling.learn.UnsupervisedClauseMarkup;
+import semanticMarkup.ling.learn.SentenceLoader;
 
 public class UnsupervisedClauseMarkupTest {
 	
 	@Test
 	public void testUnsupervisedClauseMarkup() {
-		String str = "Users/nescent/Phenoscape/TEST2/target/descriptions";
+		String str = "/Users/nescent/Phenoscape/TEST2/target/descriptions";
 		List<Treatment> treatments_l = new ArrayList<Treatment>();
 				
 		UnsupervisedClauseMarkup tester = new UnsupervisedClauseMarkup(str,"biocreative2012","plain","test");
@@ -32,7 +33,15 @@ public class UnsupervisedClauseMarkupTest {
 		assertEquals("Result", null, tester.getWordRoleTags());
 		assertEquals("Result", null, tester.getWordsToRoles());
 		assertEquals("Result", null, tester.getWordToSources());
-
+		assertEquals("Result", true, tester.populatesents());
+		
+		SentenceLoader sentLoader = new SentenceLoader(str);
+		sentLoader.load();
+		sentLoader.getUnknownWordList();
+		//assertEquals("Result", 1, sentLoader.GetType("Buckup_1998.xml_5c157037-01e4-4d48-8014-b1ebfc9dc120_8210ee00-8026-4fd9-974f-2f4cf6ce389f.txt"));
+		//assertEquals("Result", 0, sentLoader.GetType("Buckup_1998.xml_8d819b51-b88a-459e-bcb2-c6137d8b95d7.txt"));
+		
+		
 		
 		
 	}
