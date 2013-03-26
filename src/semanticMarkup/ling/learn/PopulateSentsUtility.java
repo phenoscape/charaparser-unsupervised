eb23dafd8d61a60c0f31fe389fa81e63bafa46f5
Dongye Meng <dmeng@cs.unc.edu>
2013-03-26 17:03:26 -0400

    Encapsulated all helper methods of method populateSents into a utility class named PopulateSentsUtility. Created a test class of populateSentsUtility class. Moved all test cases of those methods into the test class.

---
+++ src/semanticMarkup/ling/learn/PopulateSentsUtility.java
@@ -0,0 +1,216 @@
+package semanticMarkup.ling.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import opennlp.tools.tokenize.Tokenizer;
+
+public class PopulateSentsUtility {
+
+	private Tokenizer myTokenizer;
+
+	public PopulateSentsUtility(Tokenizer t) {
+		this.myTokenizer = t;
+	}
+
+	/**
+	 * Given a file name, return its type
+	 * 
+	 * @param fileName
+	 * @return return 1 if it is a file of character file, or 2 if it is a
+	 *         description file, otherwise return 0
+	 */
+	public int getType(String fileName) {
+		// remove pdf.xml
+		fileName = fileName.replaceAll(".*\\.xml_", "");
+		// remove all non_ charaters
+		fileName = fileName.replaceAll("[^_]", "");
+
+		// a character file
+		if (fileName.length() == 0) {
+			return 1;
+		}
+
+		// a description file
+		if (fileName.length() == 1) {
+			return 2;
+		}
+
+		return 0;
+	}
+
+	/**
+	 * replace '.', '?', ';', ':', '!' within brackets by some special markers,
+	 * to avoid split within brackets during sentence segmentation
+	 * 
+	 * @param text
+	 * @return
+	 */
+	public String hideMarksInBrackets(String text) {
+
+		if (text == null || text == "") {
+			return text;
+		}
+
+		String hide = "";
+		int lRound = 0;
+		int lSquare = 0;
+		int lCurly = 0;
+
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			switch (c) {
+			case '(':
+				lRound++;
+				hide = hide + c;
+				break;
+			case ')':
+				lRound--;
+				hide = hide + c;
+				break;
+			case '[':
+				lSquare++;
+				hide = hide + c;
+				break;
+			case ']':
+				lSquare--;
+				hide = hide + c;
+				break;
+			case '{':
+				lCurly++;
+				hide = hide + c;
+				break;
+			case '}':
+				lCurly--;
+				hide = hide + c;
+				break;
+			default:
+				if (lRound + lSquare + lCurly > 0) {
+					if (c == '.') {
+						hide = hide + "[DOT] ";
+					} else if (c == '?') {
+						hide = hide + "[QST] ";
+					} else if (c == ';') {
+						hide = hide + "[SQL] ";
+					} else if (c == ':') {
+						hide = hide + "[QLN] ";
+					} else if (c == '!') {
+						hide = hide + "[EXM] ";
+					} else {
+						hide = hide + c;
+					}
+				} else {
+					hide = hide + c;
+				}
+			}
+		}
+		return hide;
+
+	}
+
+	/**
+	 * Restore '.', '?', ';', ':', '!' within brackets
+	 * 
+	 * @param text
+	 * @return the restored string
+	 */
+	public String restoreMarksInBrackets(String text) {
+
+		if (text == null || text == "") {
+			return text;
+		}
+
+		// restore "." from "[DOT]"
+		text = text.replaceAll("\\[\\s*DOT\\s*\\]", ".");
+		// restore "?" from "[QST]"
+		text = text.replaceAll("\\[\\s*QST\\s*\\]", "?");
+		// restore ";" from "[SQL]"
+		text = text.replaceAll("\\[\\s*SQL\\s*\\]", ";");
+		// restore ":" from "[QLN]"
+		text = text.replaceAll("\\[\\s*QLN\\s*\\]", ":");
+		// restore "." from "[DOT]"
+		text = text.replaceAll("\\[\\s*EXM\\s*\\]", "!");
+
+		return text;
+	}
+
+	/**
+	 * Add space before and after all occurence of the regex in the string str
+	 * 
+	 * @param str
+	 * @param regex
+	 * @return
+	 */
+	public String addSpace(String str, String regex) {
+
+		if (str == null || str == "" || regex == null || regex == "") {
+			return str;
+		}
+
+		Matcher matcher = Pattern.compile("(^.*)(" + regex + ")(.*$)").matcher(
+				str);
+		if (matcher.lookingAt()) {
+			str = addSpace(matcher.group(1), regex) + " " + matcher.group(2)
+					+ " " + addSpace(matcher.group(3), regex);
+			return str;
+		} else {
+			return str;
+		}
+	}
+
+	/**
+	 * returns the first n words of the sentence
+	 * 
+	 * @param sent
+	 *            the sentence
+	 * @param n
+	 *            number of words to be returned
+	 * @return the first n words of the sentence. If the number of words in the
+	 *         sentence is less than n, return all of them.
+	 */
+	public List<String> getFirstNWords(String sent, int n) {
+		List<String> nWords = new ArrayList<String>();
+
+		if (sent == null || sent == "") {
+			return nWords;
+		}
+
+		String[] tokens = this.myTokenizer.tokenize(sent);
+		int minL = tokens.length > n ? n : tokens.length;
+		for (int i = 0; i < minL; i++) {
+			nWords.add(tokens[i]);
+		}
+
+		return nWords;
+	}
+
+	/**
+	 * Put all words in this sentence into the words map
+	 * 
+	 * @param sent
+	 * @param words
+	 *            a map mapping all words already known to its current count
+	 * @return a new map of all words, including words in sent
+	 */
+	public Map<String, Integer> getAllWords(String sent,
+			Map<String, Integer> words) {
+		String[] tokens = this.myTokenizer.tokenize(sent);
+
+		for (int i = 0; i < tokens.length; i++) {
+			String token = tokens[i];
+			if (words.containsKey(token)) {
+				int count = words.get(token);
+				count = count + 1;
+				words.put(token, count);
+			} else {
+				words.put(token, 1);
+			}
+		}
+
+		return words;
+	}
+
+}
---
+++ test/semanticMarkup/ling/learn/PopulateSentsUtilityTest.java
@@ -0,0 +1,130 @@
+package semanticMarkup.ling.learn;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.InvalidFormatException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PopulateSentsUtilityTest {
+	
+	private PopulateSentsUtility tester;
+	
+	@Before
+	public void initialize() {
+		// Get OpenNLP tokenizer
+		InputStream tokenModelIn;
+		Tokenizer myTokenizer = null;
+		try {
+			tokenModelIn = new FileInputStream("res/en-token.bin");
+			TokenizerModel model = new TokenizerModel(tokenModelIn);
+			myTokenizer = new TokenizerME(model);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		tester = new PopulateSentsUtility(myTokenizer);
+	}
+
+	@Test
+	public void testGetType() {
+		assertEquals("PopulateSent Helper - getType: character", 1, tester.getType("Brazeau_2009.xml_states737.txt"));
+		assertEquals("PopulateSent Helper - getType: description", 2, tester.getType("Brazeau_2009.xml_states737_state739.txt"));
+		assertEquals("PopulateSent Helper - getType: otherwise", 0, tester.getType("saf_saiflkds)dsljf_fls.txt"));
+	}
+
+	@Test
+	public void testHideMarksInBrackets() {
+		assertEquals("Result", null, 
+				tester.hideMarksInBrackets(null));
+		assertEquals("Result", "", 
+				tester.hideMarksInBrackets(""));
+		assertEquals("Result", "before (word[DOT]  word) after",
+				tester.hideMarksInBrackets("before (word. word) after"));
+		assertEquals("Result", "before (word[QST]  word) after",
+				tester.hideMarksInBrackets("before (word? word) after"));
+		assertEquals("Result", "before (word[SQL]  word) after",
+				tester.hideMarksInBrackets("before (word; word) after"));
+		assertEquals("Result", "before (word[QLN]  word) after",
+				tester.hideMarksInBrackets("before (word: word) after"));
+		assertEquals("Result", "before (word[EXM]  word) after",
+				tester.hideMarksInBrackets("before (word! word) after"));
+	}
+
+	@Test
+	public void testRestoreMarksInBrackets() {
+		assertEquals("Result", null,
+				tester.restoreMarksInBrackets(null));
+		assertEquals("Result", "",
+				tester.restoreMarksInBrackets(""));	
+		assertEquals("Result", "before (word.  word) after",
+				tester.restoreMarksInBrackets("before (word[DOT]  word) after"));	
+		assertEquals("Result", "before (word?  word) after",
+				tester.restoreMarksInBrackets("before (word[QST]  word) after"));
+		assertEquals("Result", "before (word;  word) after",
+				tester.restoreMarksInBrackets("before (word[SQL]  word) after"));
+		assertEquals("Result", "before (word:  word) after",
+				tester.restoreMarksInBrackets("before (word[QLN]  word) after"));
+		assertEquals("Result", "before (word!  word) after",
+				tester.restoreMarksInBrackets("before (word[EXM]  word) after"));
+	}
+
+	@Test
+	public void testAddSpace() {
+		// null
+				assertEquals("Result", null, tester.addSpace(null,null));
+				// ""
+				assertEquals("Result", "", tester.addSpace("", ""));
+				assertEquals("Result", "word , word ; word : word ! word ? word . ",tester.addSpace("word,word;word:word!word?word.", "\\W"));
+	}
+
+	@Test
+	public void testGetFirstNWords() {
+		List<String> nWords = new ArrayList<String>();
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords(null,-1));
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords("",-1));
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords(null, 1));
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords("", 1));
+		nWords.add("word1");
+		nWords.add("word2");
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords("word1 word2 word3 word4", 2));
+		assertEquals("PopulateSent Helper - getFirstNWords: none", nWords, tester.getFirstNWords("word1 word2", 3));
+	}
+
+	@Test
+	public void testGetAllWords() {
+		Map<String, Integer> wordsBefore = new HashMap<String, Integer>();
+		wordsBefore.put("word1", 1);
+		wordsBefore.put("word2", 2);
+		Map<String, Integer> wordsAfter = new HashMap<String, Integer>();
+		wordsAfter.put("word1", 2);
+		wordsAfter.put("word2", 4);
+		wordsAfter.put("word3", 2);
+		wordsAfter.put("word4", 1);
+		wordsAfter.put("word5", 1);
+		assertEquals("PopulateSent Helper - getAllWords", wordsAfter, tester.getAllWords("word1 word2 word3 word2 word3 word4 word5", wordsBefore));
+	}
+
+}
