18214d41315ea54e6ae85b6f7155876460ef55eb
Dongye Meng <dmeng@cs.unc.edu>
2013-05-18 15:17:38 +0800

    Moved configuration variable tagLength to Configuration class

--- src/semanticMarkup/ling/learn/Configuration.java
+++ src/semanticMarkup/ling/learn/Configuration.java
@@ -14,15 +14,18 @@ import semanticMarkup.knowledge.lib.WordNetAPI;
 
 public class Configuration {
 	
-	private String learningMode;
-	private String wordNetDictDir;
-	private String openNLPModelDir;
-	private String openNLPSentenceDetectorDir;
-	private String openNLPTokenizerDir;
+	private String learningMode = null;
+	private String wordNetDictDir = null;
+	private String openNLPModelDir = null;
+	private String openNLPSentenceDetectorDir = null;
+	private String openNLPTokenizerDir = null;
+	
+	private int tagLength = 0;
 	
 	private static final String defaultWordNetDictDir = "res/WordNet/WordNet-3.0/dict";
 	private static final String defaultOpenNLPModelDir = "res";
 	private static final String defaultLearningMode = "plain";
+	private static final int defaultTagLength = 150;
 
 	public Configuration() {
 		//this.learningMode = learningMode;
@@ -30,10 +33,21 @@ public class Configuration {
 		if (this.learningMode == null){
 			this.learningMode = Configuration.defaultLearningMode;
 		}
+		
 		this.wordNetDictDir = System.getProperty("charaparser.wordnet.dict.dir");
 		if (this.wordNetDictDir == null) {
 			this.wordNetDictDir = Configuration.defaultWordNetDictDir;
 		}
+		
+		String tempTagLength = System.getProperty("charaparser.taglength");
+		if (tempTagLength != null) {
+			this.tagLength = Integer.parseInt(tempTagLength);
+		}
+		else {
+			this.tagLength = this.defaultTagLength;
+		}
+		
+		
 		String tempOpenNLPModelDir = System.getProperty("charaparser.opennlp.model.dir");
 		if ( tempOpenNLPModelDir==null){
 			this.openNLPSentenceDetectorDir=System.getProperty("charaparser.opennlp.model.sent.dir");
