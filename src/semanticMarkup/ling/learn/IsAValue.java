37a1ea38e3da81816073c26daab76bc74b190327
Dongye Meng <dmeng@cs.unc.edu>
2013-06-10 23:23:08 -0400

    Added a new class, IsAValue, to represent the value of an entry of the isA table.

---
+++ src/semanticMarkup/ling/learn/IsAValue.java
@@ -0,0 +1,42 @@
+package semanticMarkup.ling.learn;
+
+public class IsAValue {
+	
+	private String instance;
+	private String cls;
+
+	public IsAValue(String i, String c) {
+		this.instance = i;
+		this.cls = c;
+	}
+	
+	public String getInstance() {
+		return this.instance;
+	}
+	
+	public String getCls() {
+		return this.cls;
+	}
+
+	public int hashCode() {		
+		String instanceAndClass =this.instance + this.cls; 
+		return instanceAndClass.hashCode();
+	}
+	
+	public boolean equals(Object obj){
+		if (obj==this){
+			return true;
+		}
+		
+		if (obj==null||obj.getClass()!=this.getClass()){
+			return false;
+		}
+		
+		IsAValue myIsAValue = (IsAValue) obj;
+		
+		return (   (StringUtility.equalsWithNull(this.instance, myIsAValue.getInstance()))
+				&& (this.cls == myIsAValue.getCls())
+				);
+	}
+
+}
