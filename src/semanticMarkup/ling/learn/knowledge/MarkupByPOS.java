c870be4542136285047941f4612a180a17916587
Dongye Meng <dmeng@cs.unc.edu>
2013-09-13 18:59:44 -0400

    part of the module

--- src/semanticMarkup/ling/learn/knowledge/MarkupByPOS.java
+++ src/semanticMarkup/ling/learn/knowledge/MarkupByPOS.java
@@ -8,6 +8,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import semanticMarkup.ling.learn.auxiliary.KnownTagCollection;
 import semanticMarkup.ling.learn.dataholder.DataHolder;
 import semanticMarkup.ling.learn.dataholder.SentenceStructure;
@@ -16,9 +19,12 @@ import semanticMarkup.ling.learn.utility.StringUtility;
 
 public class MarkupByPOS implements IModule {
 	private LearnerUtility myLearnerUtility;
+	private Logger myLogger;
 
 	public MarkupByPOS(LearnerUtility learnerUtility) {
 		this.myLearnerUtility = learnerUtility;
+		PropertyConfigurator.configure("conf/log4j.properties");
+		myLogger = Logger.getLogger("learn.unknownWordBootstrapping");
 	}
 
 	
@@ -42,10 +48,20 @@ public class MarkupByPOS implements IModule {
 					List<String> words = new ArrayList<String>();
 					words.addAll(Arrays.asList(sentenceItem.getSentence().split("\\s+")));
 					String ptn = this.myLearnerUtility.getSentencePtn(dataholderHandler, token, words.size()+1, words);
+					
+					if (StringUtility.isEntireMatchedNullSafe(ptn, "")) {
+						myLogger.trace("Case 1");
+					}
+					else if (StringUtility.isEntireMatchedNullSafe(ptn, "")) {
+						myLogger.trace("Case 2");
+					}
+					else {
+						myLogger.trace("Case 3");
+					}
 				}
 			}
 			
-		} while (false);
+		} while (sign > 0);
 		
 	}
 	
