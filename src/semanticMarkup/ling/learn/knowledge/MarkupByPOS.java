9df9601c36c084575e34f5d0948bb1512d15815d
Dongye Meng <dmeng@cs.unc.edu>
2013-09-19 13:34:13 -0400

    separate core part of MarkupByPOS module to a method CaseHandler()

--- src/semanticMarkup/ling/learn/knowledge/MarkupByPOS.java
+++ src/semanticMarkup/ling/learn/knowledge/MarkupByPOS.java
@@ -7,7 +7,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -49,15 +51,8 @@ public class MarkupByPOS implements IModule {
 					words.addAll(Arrays.asList(sentenceItem.getSentence().split("\\s+")));
 					String ptn = this.myLearnerUtility.getSentencePtn(dataholderHandler, token, words.size()+1, words);
 					
-					if (StringUtility.isEntireMatchedNullSafe(ptn, "")) {
-						myLogger.trace("Case 1");
-					}
-					else if (StringUtility.isEntireMatchedNullSafe(ptn, "")) {
-						myLogger.trace("Case 2");
-					}
-					else {
-						myLogger.trace("Case 3");
-					}
+					CaseHandler(dataholderHandler, sentenceItem, words, ptn);
+
 				}
 			}
 			
@@ -65,6 +60,42 @@ public class MarkupByPOS implements IModule {
 		
 	}
 	
+	public void CaseHandler(DataHolder dataholderHandler,
+			SentenceStructure sentenceItem, List<String> words, String ptn) {
+		Matcher m21 = StringUtility.createMatcher(ptn, "^([mtqb]*)([np]+)((?<=p)q)");
+		Matcher m22 = StringUtility.createMatcher(ptn, "^([mtqb]*)([np]+)(,|;|:|\\.|b)");
+		boolean case21 = m21.find();
+		boolean case22 = m22.find();
+		if (StringUtility.isMatchedNullSafe(ptn, "^[qmb][,;:\\.]$")) {
+			myLogger.trace("Case 1");
+			// tagsentwmt($sentid, $sentence, "", "ditto", "remainnulltag-[R0]");
+			dataholderHandler.tagSentenceWithMT(sentenceItem.getID(), 
+					sentenceItem.getSentence(), "", "ditto", "remainnulltag-[R0]");
+		}
+		else if (case21 || case22) {
+			myLogger.trace("Case 2");
+			int start3 = 0;
+			int end1 = 0;
+			if (case21) {
+				start3 = m21.start(3);
+				end1 = m21.end(1); 
+			}
+			else {
+				start3 = m22.start(3);
+				end1 = m22.end(1);
+			}
+			
+			String boundary = words.get(start3);
+			String modifer = StringUtils.join(words.subList(0, end1), " ");
+		}
+		else if (StringUtility.isMatchedNullSafe(ptn, "^([^qpn,;:]*)([pn]+)[tmb]")){
+			myLogger.trace("Case 3");
+		}
+		
+	}
+
+
+
 	public void tagUnknownSentences(DataHolder dataholderHandler, String mode) {
 		KnownTagCollection knownTags = myLearnerUtility.getKnownTags(dataholderHandler, mode);
 		
