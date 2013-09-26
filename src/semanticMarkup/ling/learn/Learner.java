ee9d6a75cecc621d255e97baa26cb161a6be4119
Dongye Meng <dmeng@cs.unc.edu>
2013-09-25 23:53:46 -0400

    method ditto() finished, not tested yet

--- src/semanticMarkup/ling/learn/Learner.java
+++ src/semanticMarkup/ling/learn/Learner.java
@@ -3239,12 +3239,50 @@ public class Learner {
 		dataholderHandler.updateSentenceTag("^andor$", null);
 	}
 	
-	public void markupByPOS(DataHolder dataholderHandler) {
+	public void ditto (DataHolder dataholderHandler) {
+		String nPhrasePattern = "(?:<[A-Z]*[NO]+[A-Z]*>[^<]+?<\\/[A-Z]*[NO]+[A-Z]*>\\s*)+";
+		String mPhrasePattern = "(?:<[A-Z]*M[A-Z]*>[^<]+?<\\/[A-Z]*M[A-Z]*>\\s*)+";
 		
+		for (SentenceStructure sentenceItem : dataholderHandler.getSentenceHolder()) {
+			if (sentenceItem.getTag() == null) {
+				int sentenceID = sentenceItem.getID();
+				String sentence = sentenceItem.getSentence();
+				
+			}
+		}
 	}
-
-
-
+	
+	public int dittoHelper(DataHolder dataholderHandler, int sentenceID,
+			String sentence, String nPhrasePattern, String mPhrasePattern) {
+		int res = 0;
+		String sentenceCopy = "" + sentence;
+		sentenceCopy = sentenceCopy.replaceAll("></?", "");
+		String modifier = "";
+		
+		Matcher m2 = StringUtility.createMatcher(sentenceCopy, "(.*?)"+nPhrasePattern);
+		
+		if (!StringUtility.isMatchedNullSafe(sentence, "<[NO]>")) {
+			String tag = "ditto";
+			dataholderHandler.tagSentenceWithMT(sentenceID, sentence, "", tag, "ditto-no-N");
+			res = 1;
+		}
+		else if (m2.find()) {
+			String head = m2.group(1);
+			String pattern21 = String.format("\b(%s)\b", Constant.PREPOSITION);
+			if (StringUtility.isMatchedNullSafe(head, pattern21)) {
+				String tag = "ditto";
+				dataholderHandler.tagSentenceWithMT(sentenceID, sentence, modifier, tag, "ditto-proposition");
+				res = 21;
+			}
+			else if (StringUtility.isMatchedNullSafe(head, ",<\\/B>\\s*$")) {
+				String tag = "ditto";
+				dataholderHandler.tagSentenceWithMT(sentenceID, sentence, modifier, tag, "ditto-,-N");
+				res = 22;
+			}
+		}	
+		
+		return res;
+	}
 
 
 	// some unused variables in perl
