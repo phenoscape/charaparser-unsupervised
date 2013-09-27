ce0699cb570651a0908c8ca5adc642e52c5d5591
Dongye Meng <dmeng@cs.unc.edu>
2013-09-26 20:35:05 -0400

    phraseClause and its helper. not tested yet

--- src/semanticMarkup/ling/learn/Learner.java
+++ src/semanticMarkup/ling/learn/Learner.java
@@ -3283,6 +3283,66 @@ public class Learner {
 		
 		return res;
 	}
+	
+	public void phraseClause(DataHolder dataholderHandler) {
+		for (SentenceStructure sentenceItem : dataholderHandler
+				.getSentenceHolder()) {
+			if (sentenceItem.getTag() == null) {
+				int sentenceID = sentenceItem.getID();
+				String sentence = sentenceItem.getSentence();
+				List<String> res = this.phraseClauseHelper(sentence);
+				if (res != null && res.size() == 2) {
+					String modifier = res.get(0);
+					String tag = res.get(1);
+					dataholderHandler.tagSentenceWithMT(sentenceID, sentence,
+							modifier, tag, "phraseclause");
+				}
+			}
+		}
+	}
+
+	public List<String> phraseClauseHelper(String sentence) {
+		if (sentence == null) {
+			return null;
+		}
+
+		List<String> res = new ArrayList<String>(2);
+		String pattern = "^(.*?)((?:<[A-Z]*M[A-Z]*>[^<]*?<\\/[A-Z]*M[A-Z]*>\\s*)*)((?:<[A-Z]*[NO]+[A-Z]*>[^<]*?<\\/[A-Z]*[NO]+[A-Z]*>\\s*)+)<B>[,:\\.;]<\\/B>\\s*$";
+		String sentenceCopy = "" + sentence;
+		sentenceCopy = sentenceCopy.replaceAll("></?", "");
+		
+		Matcher m = StringUtility.createMatcher(sentenceCopy, pattern);
+		if (m.find()) {
+			String head = m.group(1);
+			String modifier = m.group(2);
+			String tag = m.group(3);
+
+			String prepositionPattern = String.format("\\b(%s)\\b",
+					Constant.PREPOSITION);
+			if (!StringUtility.isMatchedNullSafe(head, prepositionPattern)
+					&& !StringUtility.isMatchedNullSafe(head, "<\\/N>")
+					&& !StringUtility.isMatchedNullSafe(modifier,
+							prepositionPattern)) {
+				if (tag != null) {
+					Matcher m2 = StringUtility.createMatcher(tag,
+							"(.*?)<N>([^<]+)<\\/N>\\s*$");
+					if (m2.find()) {
+						modifier = modifier + m2.group(1);
+						tag = m2.group(2);
+					}
+					tag = tag.replaceAll("<\\S+?>", "");
+					modifier = modifier.replaceAll("<\\S+?>", "");
+					tag = tag.replaceAll("(^\\s*|\\s*$)", "");
+					modifier = modifier.replaceAll("(^\\s*|\\s*$)", "");
+					res.add(modifier);
+					res.add(tag);
+
+					return res;
+				}
+			}
+		}
+		return res;
+	}
 
 
 	// some unused variables in perl
