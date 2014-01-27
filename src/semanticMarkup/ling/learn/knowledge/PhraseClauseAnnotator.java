ef65613bbf78d311bdad292faba9ded66492bce6
Dongye Meng <dmeng@cs.unc.edu>
2014-01-27 03:38:41 -0500

    move code of PhraseClauseAnnotation module to a new class named PhraseClauseAnnotator

---
+++ src/semanticMarkup/ling/learn/knowledge/PhraseClauseAnnotator.java
@@ -0,0 +1,23 @@
+package semanticMarkup.ling.learn.knowledge;
+
+import semanticMarkup.ling.learn.dataholder.DataHolder;
+
+/**
+ * Annotate directly the remaining clauses which are phrases with the head noun.
+ * 
+ * @author Dongye
+ * 
+ */
+public class PhraseClauseAnnotator implements IModule {
+
+	public PhraseClauseAnnotator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run(DataHolder dataholderHandler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
