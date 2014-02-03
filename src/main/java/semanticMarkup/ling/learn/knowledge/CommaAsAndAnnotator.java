8872acc44297c42c5992649b289fd4207211c877
Dongye Meng <dmeng@cs.unc.edu>
2014-02-03 11:12:00 -0500

    Added class CommaAsAndAnnotator for CommaAsAndAnnotation module

---
+++ src/main/java/semanticMarkup/ling/learn/knowledge/CommaAsAndAnnotator.java
@@ -0,0 +1,19 @@
+package semanticMarkup.ling.learn.knowledge;
+
+import semanticMarkup.ling.learn.dataholder.DataHolder;
+import semanticMarkup.ling.learn.utility.LearnerUtility;
+
+public class CommaAsAndAnnotator implements IModule {
+	private LearnerUtility myLearnerUtility;
+	
+	public CommaAsAndAnnotator(LearnerUtility learnerUtility) {
+		this.myLearnerUtility = learnerUtility;
+	}
+
+	@Override
+	public void run(DataHolder dataholderHandler) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
