fcf1b5dcf76d6ff2e8a8636dcee34378d13c31b0
Dongye Meng <dmeng@cs.unc.edu>
2014-02-17 23:58:20 -0500

    added module AdjectiveVerifier. Moved related methods into it

--- src/main/java/semanticMarkup/ling/learn/Learner.java
+++ src/main/java/semanticMarkup/ling/learn/Learner.java
@@ -38,6 +38,7 @@ import semanticMarkup.ling.learn.dataholder.WordPOSKey;
 import semanticMarkup.ling.learn.dataholder.WordPOSValue;
 import semanticMarkup.ling.learn.knowledge.AdditionalBootstrappingLearner;
 import semanticMarkup.ling.learn.knowledge.AdjectiveSubjectBootstrappingLearner;
+import semanticMarkup.ling.learn.knowledge.AdjectiveVerifier;
 import semanticMarkup.ling.learn.knowledge.AnnotationNormalizer;
 import semanticMarkup.ling.learn.knowledge.CommaAsAndAnnotator;
 import semanticMarkup.ling.learn.knowledge.Constant;
@@ -96,6 +97,8 @@ public class Learner {
 	AdditionalBootstrappingLearner additionalBootstrappingLearner;
 	
 	UnknownWordBootstrappingLearner unknownWordBootstrappingLearner;
+	
+	AdjectiveVerifier adjectiveVerifier;
 	
 	AdjectiveSubjectBootstrappingLearner adjectiveSubjectBootstrappingLearner;
 
@@ -166,6 +169,8 @@ public class Learner {
 		this.unknownWordBootstrappingLearner = new UnknownWordBootstrappingLearner(
 				this.myLearnerUtility);
 		
+		this.adjectiveVerifier = new AdjectiveVerifier(this.myLearnerUtility);
+		
 		this.adjectiveSubjectBootstrappingLearner = new AdjectiveSubjectBootstrappingLearner(this.myLearnerUtility, this.myConfiguration.getLearningMode(), this.myConfiguration.getMaxTagLength());
 		
 		this.posBasedAnnotator = new POSBasedAnnotator(this.myLearnerUtility);
@@ -224,7 +229,7 @@ public class Learner {
 		this.unknownWordBootstrappingLearner.run(myDataHolder);
 
 		myLogger.info("Adjectives Verification:");
-		this.adjectivesVerification(myDataHolder); // !!!
+		this.adjectiveVerifier.run(myDataHolder);
 
 		this.separateModifierTag(myDataHolder); // !!!
 
@@ -2190,112 +2195,9 @@ public class Learner {
 
 
 
-	/**
-	 * correct markups that used an adj as an s, e.g lateral, adult, juvenile
-	 */
-	public void adjectivesVerification(DataHolder dataholderHandler) {
-		PropertyConfigurator.configure("conf/log4j.properties");
-		Logger myLogger = Logger.getLogger("learn.adjectivesVerification");
 
-		String pattern = "^<N>([a-z]+)</N> ([^N,;.]+ <N>[a-z]+</N>)";
-		Iterator<SentenceStructure> iter = dataholderHandler
-				.getSentenceHolderIterator();
-		while (iter.hasNext()) {
-			SentenceStructure sentenceItem = iter.next();
-			String sentence = sentenceItem.getSentence();
-			// System.out.println(sentence);
-			Set<Integer> ids = new HashSet<Integer>();
-			ids.add(133);
-			ids.add(163);
-			ids.add(236);
-			ids.add(269);
 
-			if (ids.contains(sentenceItem.getID())) {
-				System.out.println();
-			}
 
-			if (sentence != null) {
-				Pattern p = Pattern.compile(pattern);
-				Matcher m = p.matcher(sentence);
-				if (m.find()) {
-					String part1 = m.group(1);
-					String part2 = m.group(2);
-					myLogger.trace(String.format("Sentence %s\n"
-							+ "\tSentence: %s\n" + "\tPart1: %s\n"
-							+ "\tPart2: %s", sentenceItem.getID(),
-							sentenceItem.getSentence(), part1, part2));
-					boolean condition1 = this.isSentenceTag(dataholderHandler,
-							part2);
-					boolean condition2 = StringUtils.equals(this
-							.getLearnerUtility().getWordFormUtility()
-							.getNumber(part1), "p");
-
-					if (condition1 && condition2) {
-						String wrongWord = part1;
-						myLogger.trace("\tWrong: " + wrongWord);
-						// if (StringUtility.isMatchedNullSafe(wrongWord,
-						// "\\w")) {
-						if (StringUtility.isMatchedNullSafe(wrongWord, "\\w")) {
-							this.noun2Modifier(dataholderHandler, wrongWord);
-							Set<String> words = dataholderHandler
-									.getWordsFromUnknownWord(null, false,
-											String.format("^%s$", wrongWord),
-											true);
-							for (String word : words) {
-								this.noun2Modifier(dataholderHandler, word);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Check if a word is (part of) the tag of any sentence
-	 * 
-	 * @param dataholderHandler
-	 *            DataHolder handler
-	 * @param raw
-	 *            word to check
-	 * @return true if it is, false otherwise
-	 */
-	public boolean isSentenceTag(DataHolder dataholderHandler, String raw) {
-		boolean result = false;
-		result = dataholderHandler.isExistSentence(false,
-				String.format("^%s.*$", raw));
-
-		return result;
-	}
-
-	/**
-	 * change the POS tag of a word from noun to modifier
-	 * 
-	 * @param dataholderHandler
-	 *            dataholder handler
-	 * 
-	 * @param word
-	 *            the word to change
-	 * @return true if any updates has been made, false otherwise
-	 */
-	public boolean noun2Modifier(DataHolder dataholderHandler, String word) {
-		boolean isUpdated = false;
-
-		ArrayList<String> deletedPOSs = new ArrayList<String>();
-		deletedPOSs.add("s");
-		deletedPOSs.add("p");
-		deletedPOSs.add("n");
-
-		for (String POS : deletedPOSs) {
-			dataholderHandler.deleteWordPOS(true, word, true, POS);
-		}
-		dataholderHandler.updateDataHolder(word, "m", "", "modifiers", 1);
-
-		String oldPattern = String.format("(^%s$|^.* %s$)", word, word);
-		dataholderHandler.updateSentenceTag(oldPattern, null);
-
-		return isUpdated;
-	}
 
 	public void separateModifierTag(DataHolder dataholderHandler) {
 		PropertyConfigurator.configure("conf/log4j.properties");
